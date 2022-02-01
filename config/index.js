bf9c836b6c8cb6322a056b10a88ae2d3e4d71fc7
Konneka <aince@konneka.com>
2022-02-01 10:45:10 +0300

    add config/ module

---
+++ config/index.js
@@ -0,0 +1,12 @@
+const path = require("path")
+const args = require('minimist')(process.argv.slice(2), { string: ["phone-number"] })
+const dotenv = require("dotenv").config({path: path.join(__dirname, "../.env")});
+const _env = dotenv.parsed;
+
+const config = require("./config.json");
+console.log({args})
+module.exports = () => ({
+    ...config,
+    ..._env,
+    args
+})
